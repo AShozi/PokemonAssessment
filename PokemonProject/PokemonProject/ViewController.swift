7f3c1cc6db16f7f23a35382d06122711791a8523
AShozi <AShozi@dvtsoftware.com>
2024-07-23 09:46:56 +0200

    initial commit

---
+++ PokemonProject/PokemonProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonProject
+//
+//  Created by Aphiwe Shozi on 2024/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
