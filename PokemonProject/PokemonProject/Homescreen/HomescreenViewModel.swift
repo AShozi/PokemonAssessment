86d1ac859d8e3ec83cf39c7309aa5094a38607b5
AShozi <AShozi@dvtsoftware.com>
2024-07-23 13:46:58 +0200

    code clean up

--- PokemonProject/PokemonProject/Homescreen/HomescreenViewModel.swift
+++ PokemonProject/PokemonProject/Homescreen/HomescreenViewModel.swift
@@ -30,18 +30,26 @@ class HomescreenViewModel{
     
     //functions
     
+    func reloadView(){
+        
+    }
+    
+    func show(){
+        
+    }
+    
     func pokeAtIndex(atIndex:Int) -> poke {
         allPokeList[atIndex]
     }
 //dont stress u know this you can still clean
     
-    func fetchHomeResult() { [weak self] result in 
+    func fetchHomeResult() { [weak self] result in
         switch result {
     case .success(let object):
-        self.allPokeList = object
-        self.delegate.reloadView()
+            self.allPokeList = object
+            self.delegate?.reloadView()
     case .failure (let error):
-        self.delegate.show(error.rawValue)
+            self.delegate?.show(error.rawValue)
     }
         
         
