0217644f326f94005f68433e205654fd64f4b778
AShozi <AShozi@dvtsoftware.com>
2024-07-23 13:30:39 +0200

    initialised MVVM

---
+++ PokemonProject/PokemonProject/InfoScreen/InfoscreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InfoscreenViewController.swift
+//  PokemonProject
+//
+//  Created by Aphiwe Shozi on 2024/07/23.
+//
+
+import UIKit
+
+class InfoscreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
