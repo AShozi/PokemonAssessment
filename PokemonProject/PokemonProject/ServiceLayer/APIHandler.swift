5c3a4eeb8f243c9913befce6e3cb791ddda3ac34
AShozi <AShozi@dvtsoftware.com>
2024-07-25 10:21:52 +0200

    debugged service layer completion

--- PokemonProject/PokemonProject/ServiceLayer/APIHandler.swift
+++ PokemonProject/PokemonProject/ServiceLayer/APIHandler.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension URLSession {
-    func request <T:Codable> (endpoint:String,method:Method,completion:((Result <T,APIError>) -> Void)) {
+    func request <T:Codable> (endpoint:String,method:Method,completion: @escaping ((Result <T,APIError>) -> Void)) {
         guard let url = URL (string:endpoint) else {
             completion(.failure(.internalError))
             return
@@ -20,7 +20,7 @@ extension URLSession {
         
     }
     
-    private func call <T:Codable> (with request: URLRequest,completion:((Result <T,APIError>) -> Void)) {
+    private func call <T:Codable> (with request: URLRequest,completion: @escaping((Result <T,APIError>) -> Void)) {
         let dataTask = URLSession.shared.dataTask (with: request) { data, _, error in
             guard error == nil else {
                 DispatchQueue.main.async{
